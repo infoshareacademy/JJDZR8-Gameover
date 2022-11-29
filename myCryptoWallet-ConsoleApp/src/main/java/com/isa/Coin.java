53b4e0a43803ca01617a23d1c1b14b6360e50013
Lidia Cholewa <lidia.cholewa@wp.pl>
2022-11-29 18:18:54 +0100

    add new set of class

---
+++ myCryptoWallet-ConsoleApp/src/main/java/com/isa/Coin.java
@@ -0,0 +1,67 @@
+package com.isa;
+
+public class Coin {
+    String coinName;
+    String coinSymbol;
+    Double price;
+    Enum currency;
+    Double coinVolumen;
+    String description;
+    Double changePrice;
+
+    public String getCoinName() {
+        return coinName;
+    }
+
+    public void setCoinName(String coinName) {
+        this.coinName = coinName;
+    }
+
+    public String getCoinSymbol() {
+        return coinSymbol;
+    }
+
+    public void setCoinSymbol(String coinSymbol) {
+        this.coinSymbol = coinSymbol;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Enum getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Enum currency) {
+        this.currency = currency;
+    }
+
+    public Double getCoinVolumen() {
+        return coinVolumen;
+    }
+
+    public void setCoinVolumen(Double coinVolumen) {
+        this.coinVolumen = coinVolumen;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getChangePrice() {
+        return changePrice;
+    }
+
+    public void setChangePrice(Double changePrice) {
+        this.changePrice = changePrice;
+    }
+}
