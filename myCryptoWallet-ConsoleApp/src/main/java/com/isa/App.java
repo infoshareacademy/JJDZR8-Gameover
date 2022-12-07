7a7341f2f94437fec16a754127628e483a205a93
Eryk Romanowski <erykromanowskii@gmail.com>
2022-12-07 18:45:26 +0100

    edited app calss

--- myCryptoWallet-ConsoleApp/src/main/java/com/isa/App.java
+++ myCryptoWallet-ConsoleApp/src/main/java/com/isa/App.java
@@ -2,7 +2,9 @@ package com.isa;
 
 public class App {
 
-    public static void main(String[] args) {
+        public static void main( String[] args )
+        {
 
-    }
+            System.out.println( "Created by GameOver Team" );
+        }
 }
