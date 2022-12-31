2262944f27fc16b0c0420a6b46886084e0342f82
Juliusz Makowski <julomaki@gmail.com>
2022-12-31 13:12:47 +0100

    CoinsList class added.

---
+++ myCryptoWallet-ConsoleApp/src/main/java/com/isa/CoinsList.java
@@ -0,0 +1,14 @@
+package com.isa;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CoinsList {
+
+    private Map<Integer, List<String>> pagesMap;
+    private ArrayList<String> coins;
+    private int totalPages;
+    private int recordsPerPage;
+
+}
