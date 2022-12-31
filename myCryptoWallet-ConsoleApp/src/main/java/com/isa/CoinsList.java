9e38f489fc9f4b40d7d0e5c700e64207a8a3901a
Juliusz Makowski <julomaki@gmail.com>
2022-12-31 14:24:53 +0100

    pagesCreator and openPage method in CoinsList class added

--- myCryptoWallet-ConsoleApp/src/main/java/com/isa/CoinsList.java
+++ myCryptoWallet-ConsoleApp/src/main/java/com/isa/CoinsList.java
@@ -1,8 +1,6 @@
 package com.isa;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CoinsList {
 
@@ -11,4 +9,43 @@ public class CoinsList {
     private int totalPages;
     private int recordsPerPage;
 
+
+    public void pagesCreator() {
+        this.pagesMap = new LinkedHashMap<>();
+
+        if (coins.size() < recordsPerPage) this.totalPages = 1;
+        else if (coins.size() % recordsPerPage != 0) this.totalPages = coins.size() / recordsPerPage + 1;
+        else this.totalPages = coins.size() / recordsPerPage;
+
+        for (int i = 0; i < totalPages; i++) {
+            List<String> temporaryCoinList;
+            if (coins.size() < recordsPerPage) temporaryCoinList = coins.subList(0, coins.size());
+            else if (i == totalPages - 1) {
+                temporaryCoinList = coins.subList(i * recordsPerPage, coins.size());
+            } else temporaryCoinList = coins.subList(i * recordsPerPage, (i + 1) * recordsPerPage);
+
+            pagesMap.put(i, temporaryCoinList);
+        }
+    }
+
+    public List<String> openPage(Integer pageNumber){
+        return pagesMap.get(pageNumber - 1);
+    }
+
+    public List<String> openPageFromKeyboard(){
+        Scanner scanner = new Scanner(System.in);
+        Integer pageNumber = 0;
+        do {
+            pageNumber = Integer.parseInt(scanner.nextLine());
+            return openPage(pageNumber);
+        }while(pageNumber > 0 && pageNumber <= totalPages);
+
+
+
+    }
+
+
+    public Map<Integer, List<String>> getPagesMap() {
+        return pagesMap;
+    }
 }
