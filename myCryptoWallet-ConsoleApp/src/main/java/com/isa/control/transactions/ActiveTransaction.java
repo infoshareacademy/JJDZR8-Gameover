8f2659e0ab8f56e2f74e26f050d2976bd14c4cb7
Juliusz Makowski <julomaki@gmail.com>
2023-02-04 21:19:08 +0100

    equals and hashcode method added, Transactions implements Comparable<>.

--- myCryptoWallet-ConsoleApp/src/main/java/com/isa/control/transactions/ActiveTransaction.java
+++ myCryptoWallet-ConsoleApp/src/main/java/com/isa/control/transactions/ActiveTransaction.java
@@ -8,6 +8,7 @@ import com.isa.control.Endpoints;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 public class ActiveTransaction extends Transactions implements Transaction{
 
@@ -73,5 +74,17 @@ public class ActiveTransaction extends Transactions implements Transaction{
         return currentPrice;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ActiveTransaction that = (ActiveTransaction) o;
+        return Double.compare(that.openPrice, openPrice) == 0 && Double.compare(that.currentPrice, currentPrice) == 0 && Objects.equals(openTransactionDate, that.openTransactionDate);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), openTransactionDate, openPrice, currentPrice);
+    }
 }
--- myCryptoWallet-ConsoleApp/src/main/java/com/isa/control/transactions/Transactions.java
+++ myCryptoWallet-ConsoleApp/src/main/java/com/isa/control/transactions/Transactions.java
@@ -3,7 +3,9 @@ package com.isa.control.transactions;
 import com.isa.control.Coin;
 import com.isa.control.Endpoints;
 
-public class Transactions {
+import java.util.Objects;
+
+public class Transactions implements Comparable<Transactions>{
     private final long idTransaction;
     private final Coin coin;
     private final boolean isActive;
@@ -33,5 +35,26 @@ public class Transactions {
     }
     public boolean checkEndpointsName(){
         return Endpoints.getCoinsNames().containsKey(coin.getShortSymbol());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Transactions that = (Transactions) o;
+        return idTransaction == that.idTransaction && isActive == that.isActive && Double.compare(that.volume, volume) == 0 && Objects.equals(coin, that.coin);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idTransaction, coin, isActive, volume);
+    }
+
+    @Override
+    public int compareTo(Transactions transactions) {
+        if(idTransaction<transactions.getIdTransaction()) return 1;
+        else if (idTransaction>transactions.getIdTransaction()) return -1;
+        else return 0;
+
     }
 }
