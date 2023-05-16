efd753dc5054197162a91c2f3d106dd12f162fbb
Juliusz Makowski <julomaki@gmail.com>
2023-05-16 19:15:16 +0200

    LocalResolver added

--- myCryptoWallet-WebApp/src/main/java/com/isa/config/CryptoWalletConfig.java
+++ myCryptoWallet-WebApp/src/main/java/com/isa/config/CryptoWalletConfig.java
@@ -3,12 +3,18 @@ package com.isa.config;
 import com.isa.control.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 @Configuration
-public class CryptoWalletConfig {
+public class CryptoWalletConfig implements WebMvcConfigurer {
 
     @Bean
     public Wallet wallet(){
@@ -18,6 +24,22 @@ public class CryptoWalletConfig {
     @Bean
     public Coin coin(){return  new Coin();}
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.ENGLISH);
+        return slr;
+    }
 
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }
